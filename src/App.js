ee4344eb0224b6acf69867204de7aad5a09bd77c
ISPserver <tjdals080@naver.com>
2019-08-17 20:10:06 +0900

    Movie Information Web && Component State

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component }from 'react';
+import './App.css';
+import Movie from './Movie';
+
+const movies = [
+  {
+    title: "Matrix",
+    poster: "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg"
+  },
+  {
+    title: "Full Metal Jacket",
+    poster: "https://upload.wikimedia.org/wikipedia/en/9/99/Full_Metal_Jacket_poster.jpg"
+  },
+  {
+    title: "Oldboy",
+    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg"
+  },
+  {
+    title: "Star Wars",
+    poster: "http://ae01.alicdn.com/kf/UTB8DYAekqrFXKJk43Ovq6ybnpXax/Vintage-Star-Wars-Poster-i-want-you-Retro-art-Wall-home-Decoration-Movie-poster-Wall-stickers.jpg"
+  }
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {movies.map((movie, index) => {
+          return <Movie title={movie.title} poster={movie.poster} key={index}/>
+        })}
+      </div>
+    )
+  }
+}
+
+export default App;
